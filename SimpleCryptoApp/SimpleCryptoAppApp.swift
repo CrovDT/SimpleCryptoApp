5910bcead888c818d72ec3138d61c816606f4dda
Daniil Tretyak <tretuakdaniil@gmail.com>
2023-01-10 11:13:05 +0200

    Initial Commit

---
+++ SimpleCryptoApp/SimpleCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCryptoAppApp.swift
+//  SimpleCryptoApp
+//
+//  Created by Daniil Tretyak on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
